1353e9dba4ae4d7544c34ce0bc395e46b535a06f
JYL <leejiayi@gmail.com>
2018-03-18 02:25:10 +0800

    Improvements to ETL

--- bikeshare/src/main/scala/team4/bikeshare/project/BigBikeGraph.scala
+++ bikeshare/src/main/scala/team4/bikeshare/project/BigBikeGraph.scala
@@ -8,18 +8,21 @@ import org.apache.spark.sql.SparkSession
 object BikeShareApp {
   def main(args: Array[String]) {
     val sparkSession = SparkSession.builder.master("local").appName("spark session example").getOrCreate()
-    val df = sparkSession.read.option("header","true").csv("src/main/resources/20180102-capitalbikeshare-tripdata.csv")
-    // df.printSchema()
-    // df.show()
-    // val start_stations = df.select("start_station_number", "Start station").distinct
-    val start_stations = df.selectExpr("cast(start_station_number as int) start_station_number", "start_station").distinct
+    val df = sparkSession.read.option("header","true").csv("src/main/resources/2016Q4-capitalbikeshare-tripdata.csv")
+    var newDf = df
+    for(col <- df.columns){
+      newDf = newDf.withColumnRenamed(col,col.replaceAll("\\s", "_"))
+    }
+    // newDf.printSchema()
+    // newDf.show()
+    val start_stations = newDf.selectExpr("cast(Start_station_number as int) Start_station_number", "Start_station").distinct
     val start_stations_rdd = start_stations.rdd
-    val end_stations = df.selectExpr("cast(end_station_number as int) end_station_number", "end_station").distinct
+    val end_stations = newDf.selectExpr("cast(End_station_number as int) End_station_number", "End_station").distinct
     val end_stations_rdd = end_stations.rdd
     val all_stations_rdd = start_stations_rdd.union(end_stations_rdd).distinct
     // all_stations_rdd.take(10).foreach(println)
     // println(">> Total number of stations : " + all_stations_rdd.count())
-    val trips = df.selectExpr("cast(start_station_number as int) start_station_number", "cast(end_station_number as int) end_station_number").distinct
+    val trips = newDf.selectExpr("cast(Start_station_number as int) Start_station_number", "cast(End_station_number as int) End_station_number").distinct
     val trips_rdd = trips.rdd
     // // Create an RDD for the vertices
     val station_vertices: RDD[(VertexId, String)] = all_stations_rdd.map(row => (row(0).asInstanceOf[Number].longValue, row(1).asInstanceOf[String]))
@@ -34,8 +37,6 @@ object BikeShareApp {
     println("Total Number of Trips: " + station_graph.numEdges)
     // sanity check
     println("Total Number of Trips in Original Data: " + trips_rdd.count)
-    // println("Total Number of Postdocs: " + graph.vertices.filter { case (id, (name, pos)) => pos == "postdoc" }.count)
-    // println("Total Number of Stations: " + graph.numVertices)
     sparkSession.stop()
   }
 }
