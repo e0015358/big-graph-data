2f86e5839a17038d7726ff0cd4450af572ded85e
jyl <jiayi.lee@docomointertouch.com>
2018-04-11 17:53:23 +0800

    Output top bike sources to CSV file

--- bikeshare/src/main/scala/team3/bikeshare/project/BigBikeGraphDegrees.scala
+++ bikeshare/src/main/scala/team3/bikeshare/project/BigBikeGraphDegrees.scala
@@ -1,11 +1,12 @@
 package team3.bikeshare.project
-
 import org.apache.spark.graphx._
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
+import org.apache.spark.SparkContext._
+import java.io._
 
 
 object BigBikeGraphOutDegreesInDegrees {
@@ -14,6 +15,7 @@ object BigBikeGraphOutDegreesInDegrees {
     Logger.getLogger("akka").setLevel(Level.OFF)
     val sparkSession = SparkSession.builder.master("local").appName("Bike Share Vertex Degrees").getOrCreate()
     sparkSession.conf.set("spark.executor.memory", "2g")
+    val sc = sparkSession.sparkContext
     val df = sparkSession.read.option("header","true").csv("src/main/resources/201801_fordgobike_tripdata.csv")
     val newDf = df.sample(false, 0.05)
     println("Processing "+ newDf.count + " datapoints")
@@ -43,12 +45,14 @@ object BigBikeGraphOutDegreesInDegrees {
     // sanity check
     println("Total Number of Trips in Original Data: " + trips_rdd.count)
     println("="*70)
-    val inDegrees: VertexRDD[Int] = station_graph.inDegrees
-    println("In Degrees : " + inDegrees)
-    println("="*70)
-    val maxInDegree: (VertexId, Int) = station_graph.inDegrees.reduce(max)
-    println("Max In Degrees : " + maxInDegree)
-    println("="*70)
+
+    // val inDegrees: VertexRDD[Int] = station_graph.inDegrees
+    // // println("In Degrees : " + inDegrees)
+    // // println("="*70)
+    // val maxInDegree: (VertexId, Int) = station_graph.inDegrees.reduce(max)
+    // println("Max In Degrees : " + maxInDegree)
+    // println("="*70)
+    //
     // station_graph
     //     .groupEdges((edge1, edge2) => edge1 + edge2)
     //     .triplets
@@ -58,37 +62,52 @@ object BigBikeGraphOutDegreesInDegrees {
     //     .take(10)
     //     .foreach(println)
     // println("="*70)
-    station_graph
+
+    // Bike stations with the most inboud traffic
+    val top_inbound = station_graph
         .inDegrees
         .join(station_vertices)
         .sortBy(_._2._1, ascending=false)
         .take(10)
-        .foreach(x => println(x._2._2 + " has " + x._2._1 + " in degrees."))
+    top_inbound.foreach(x => println(x._2._2 + " has " + x._2._1 + " in degrees."))
     println("="*70)
-    station_graph
+
+    // Bike stations with the most outbound traffic
+    val top_outbound = station_graph
         .outDegrees
         .join(station_vertices)
         .sortBy(_._2._1, ascending=false)
         .take(10)
-        .foreach(x => println(x._2._2 + " has " + x._2._1 + " out degrees."))
+    top_outbound.foreach(x => println(x._2._2 + " has " + x._2._1 + " out degrees."))
     println("="*70)
-    station_graph
+
+    // Here we discover which stations have the highest ratio of in degrees but least out degrees
+    val top_sinks = station_graph
         .inDegrees
         .join(station_graph.outDegrees) // join with out Degrees
         .join(station_vertices) // join with our other stations
         .map(x => (x._2._1._1.toDouble/x._2._1._2.toDouble, x._2._2)) // ratio of in to out
-        .sortBy(_._1, ascending=false)
-        .take(5)
-        .foreach(x => println(x._2 + " has a in/out degree ratio of " + x._1))
+        .sortBy(_._1, ascending=false) // sort by high to low
+        .take(10)
+    top_sinks.foreach(x => println(x._2 + " has a in/out degree ratio of " + x._1))
     println("="*70)
-    station_graph
+
+    // Here we discover which stations have the highest ratio of out degrees vs in degrees
+    val top_sources = station_graph
         .inDegrees
         .join(station_graph.outDegrees) // join with out Degrees
         .join(station_vertices) // join with our other stations
-        .map(x => (x._2._1._1.toDouble/x._2._1._2.toDouble, x._2._2)) // ratio of in to out
-        .sortBy(_._1)
-        .take(5)
-        .foreach(x => println(x._2 + " has a in/out degree ratio of " + x._1))
+        .map(x => (x._2._1._2.toDouble/x._2._1._1.toDouble, x._2._2)) // ratio of out to in
+        .sortBy(_._1, ascending=false) // sort by high to low
+        .take(100)
+    top_sources.foreach(x => println(x._2 + " has a out/in degree ratio of " + x._1))
+    // val writer = new BufferedWriter(new FileWriter(top_sources_file))
+    // top_sources.foreach(writer.write)
+    // writer.close()
+    // sc.parallelize(top_sources).saveAsTextFile(top_sources_file)
+    val top_sources_file = "top_sources"
+
+    sc.parallelize(top_sources.map(tuple => "%s,%s".format(tuple._2, tuple._1))).saveAsTextFile(top_sources_file)
     sparkSession.stop()
   }
 
